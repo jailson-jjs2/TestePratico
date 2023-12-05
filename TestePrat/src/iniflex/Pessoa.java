444bb7bc8b5ffb8d1863eaf24a5108b3f6db940d
JailsonJJS2 <jjs2@hotmail.com>
2023-12-05 13:39:47 -0300

    Enviando teste

---
+++ TestePrat/src/iniflex/Pessoa.java
@@ -0,0 +1,29 @@
+package iniflex;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+	
+	private String nome;
+	
+	private LocalDate dataNasc;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(LocalDate dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	
+	
+
+}
